68667e505ab39b02facffb1828fbb5e0e492d50d
lvqinghao <lvqinghao@163.com>
2021-01-20 21:56:27 +0800

    v3 commit

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,25 @@ use std::ops::{Div, Mul, AddAssign, DivAssign, Sub, MulAssign};
 use bigdecimal::BigDecimal;
 use std::str::FromStr;
 
-pub fn matrix_init(n: usize, m: usize, k: usize, t: usize, r: usize) -> Vec<Vec<Vec<BigUint>>> {
-    // assert!(n > 0 && m > 0 && k > 0);
+pub fn comb_array_init(k: usize, t: usize) -> Vec<BigUint> {
+    assert!(k > 0 && t > 0);
+    let mut comb_array = vec![BigUint::from(0u32); k+1];
+    comb_array[1] = BigUint::from(1u32);
+    for i in 2..=k {
+        comb_array[i] = combination(i*t, t).mul(&comb_array[i-1]);
+    }
+    comb_array
+}
+
+pub fn matrix_init(n: usize, m: usize, k: usize, t: usize, r: usize, comb_array: &Vec<BigUint>) -> Vec<Vec<Vec<BigUint>>> {
+    assert!(k > 0 && t > 1 && n+m == k*t);
     let mut matrix = vec![vec![vec![BigUint::from(0u32); k+1]; m+1]; n+1];
     for i_k in 1..=k {
         let volume = i_k*t;
         for i_n in 0..=n {
             for i_m in 0..=m {
                 if i_n+i_m == volume && i_n <= r {
-                    let mut ret = BigUint::from(1u32);
-                    for j in 2..=i_k {
-                        ret.mul_assign(combination(j*t, t));
-                    }
-                    matrix[i_n][i_m][i_k] = ret.clone();
+                    matrix[i_n][i_m][i_k] = comb_array[i_k].clone();
                     // println!("matrix[{}][{}][{}] is {}", i_n, i_m, i_k, matrix[i_n][i_m][i_k]);
                 }
             }
@@ -32,7 +38,7 @@ pub fn matrix_init(n: usize, m: usize, k: usize, t: usize, r: usize) -> Vec<Vec<
 
 pub fn partition(
     n: usize, m: usize, k: usize, t: usize, r: usize,
-    matrix: &mut Vec<Vec<Vec<BigUint>>>, sum: &mut BigUint, hited: &mut BigUint
+    matrix: &mut Vec<Vec<Vec<BigUint>>>, sum: &mut BigUint, hit: &mut BigUint
 ) -> BigUint {
     // println!("#### Begin #### n = {}, m = {}, k = {}, t = {}, r = {}", n, m, k, t, r);
     assert!(k > 0 && (n > 0 || m > 0));
@@ -41,14 +47,14 @@ pub fn partition(
     sum.add_assign(&one);
     let mut ret = BigUint::from(0u32);
     if matrix[n][m][k].ne(&ret) {
-        hited.add_assign(&one);
+        hit.add_assign(&one);
         ret = matrix[n][m][k].clone();
     } else {
         for i in 0..=r {
             if n >= i && m >= t-i && r*(k-1) >= n-i {
                 let factor = combination(n, i).mul(combination(m, t-i));
                 // println!("Recall** n = {}, i = {}, m = {}, j = {}, factor = {}", n, i, m, t-i, factor);
-                ret.add_assign(factor.mul(partition(n-i, m+i-t, k-1, t, r, matrix, sum, hited)));
+                ret.add_assign(factor.mul(partition(n-i, m+i-t, k-1, t, r, matrix, sum, hit)));
             }
         }
         matrix[n][m][k] = ret.clone();
@@ -125,24 +131,28 @@ fn test_combination_easy() {
 
 
 fn main() {
-    let N = 333;
+    let nn = 333;
     let mut m;
     let k = 100;
     let t = 10;
     let r = 5;
-    for n in 0..=N {
-        let mut sum = BigUint::from(0u32);
-        let mut hited = BigUint::from(0u32);
-        m = 1000 - n;
-        let mut matrix = matrix_init(n, m, k, t, r);
-        let ret = partition(n, m, k, t, r, &mut matrix, &mut sum, &mut hited);
-        // println!("Count is  {}", ret);
 
-        let mut total = BigUint::from(1u32);;
-        for i in 2..=k {
-            total.mul_assign(combination(i * t, t));
-        }
-        // println!("total is  {}", total);
+    let mut total = BigUint::from(1u32);
+    for i in 2..=k {
+        total.mul_assign(combination(i*t, t));
+    }
+    println!("total is  {}", total);
+    let comb_array = comb_array_init(k, t);
+    println!("comb_array[{}] is  {}", k, comb_array[k]);
+    assert!(total.eq(&comb_array[k]));
+
+    for n in 0..=nn {
+        let mut sum = BigUint::from(0u32);
+        let mut hit = BigUint::from(0u32);
+        m = 1000 - n;
+        let mut matrix = matrix_init(n, m, k, t, r, &comb_array);
+        let ret = partition(n, m, k, t, r, &mut matrix, &mut sum, &mut hit);
+        // println!("Count is  {}", ret);
 
         let tmp = format!("{}", ret);
         let up = BigDecimal::from_str(&tmp).unwrap();
@@ -155,7 +165,7 @@ fn main() {
         let p = up.div(&down);
         let one = BigDecimal::from_str("1").unwrap();
         println!("#### n = {}", n);
-        println!("==== total called {}, hited {}", sum, hited);
+        println!("==== total called {}, hit {}", sum, hit);
         println!("OK, {}", p);
         println!("ERR, {}", one.sub(&p));
     }
